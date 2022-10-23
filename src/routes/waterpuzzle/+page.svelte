9af076168f43ea08e1d053865215a147c523b452
JonathanGodar <jonathan.godar@gmail.com>
2022-10-23 14:18:32 +0200

    more water puzzle

--- src/routes/waterpuzzle/+page.svelte
+++ src/routes/waterpuzzle/+page.svelte
@@ -1,17 +1,154 @@
 <script lang="ts">
+import type { liquid } from "./flask";
 import Flask from "./flask.svelte";
-import type { flask } from "./flask";
 
-let flasks: flask[] = [{
-	liquids: {
-			id: 1,
-			ammount: 10,
-	}
-}];
+let flasks: liquid[][] = [
+	[
+		{
+				id: 1,
+				ammount: 10,
+		},
+		{
+				id: 2,
+				ammount: 50,
+		}
+	],
+	[
+		{
+				id: 2,
+				ammount: 50,
+		},
+		{
+				id: 3,
+				ammount: 50,
+		}
+	],
+	[
+		{
+				id: 2,
+				ammount: 50,
+		},
+		{
+				id: 3,
+				ammount: 50,
+		} ],
+
+	[
+		{
+				id: 2,
+				ammount: 50,
+		},
+		{
+				id: 3,
+				ammount: 50,
+		}
+	],
+
+	[
+		{
+				id: 2,
+				ammount: 50,
+		},
+		{
+				id: 3,
+				ammount: 50,
+		}
+	],
+
+	[
+		{
+				id: 2,
+				ammount: 50,
+		},
+		{
+				id: 3,
+				ammount: 50,
+		}
+	],
+
+	[
+		{
+				id: 2,
+				ammount: 50,
+		},
+		{
+				id: 3,
+				ammount: 50,
+		}
+	],
+
+	[
+		{
+				id: 2,
+				ammount: 50,
+		},
+		{
+				id: 3,
+				ammount: 50,
+		}
+	],
+
+	[
+		{
+				id: 2,
+				ammount: 50,
+		},
+		{
+				id: 3,
+				ammount: 50,
+		}
+	],
+
+	[
+		{
+				id: 2,
+				ammount: 50,
+		},
+		{
+				id: 3,
+				ammount: 50,
+		}
+	],
+
+	[
+		{
+				id: 2,
+				ammount: 50,
+		},
+		{
+				id: 3,
+				ammount: 50,
+		}
+	],
+
+	[
+		{
+				id: 2,
+				ammount: 50,
+		},
+		{
+				id: 3,
+				ammount: 50,
+		}
+	],
+
+];
 
 </script>
 
-{#each flasks as flask }
-	<Flask me={flask}/>
+<div class="shelf">
+{#each flasks as flask}
+	<Flask contents={flask}/>
 {/each}
+</div>
 
+
+<style>
+.shelf {
+	display: flex; 
+	height: 200px;
+	justify-content: space-around;
+	flex-wrap: wrap;
+}
+
+</style>
--- src/routes/waterpuzzle/flask.svelte
+++ src/routes/waterpuzzle/flask.svelte
@@ -1,7 +1,39 @@
 <script lang="ts">
-import type { flask } from "./flask";
-export let me: flask;
+import { liquid_colors, type liquid } from "./flask";
+
+export let contents: liquid[];
+
+
+$: air_size = 100 - contents.map((v) => v.ammount).reduce((p, v) => p + v);
+$: () => {
+	console.log(air_size);
+}
 </script>
 
+<div class="flask">
+{#each contents as liquid }
+	<div class="liquid" style={`flex-grow: ${liquid.ammount}; background-color: ${liquid_colors[liquid.id]}`}></div>
+{/each}
+<div style={`flex-grow: ${air_size}; background-color: rgba(0, 0, 0, 0);`}></div>
+</div>
 
-{me.liquids.ammount}
+
+
+
+<style>
+.flask {
+	display: flex;
+	flex-direction: column-reverse;
+	border-left: 2px solid black;
+	border-right: 2px solid black;
+	border-bottom: 2px solid black;
+	min-width: 20px;
+	margin: 0.7em 0.5rem;
+}
+
+/* .liquid { */
+/* 	/1* border-top: 2px solid gray; *1/ */
+/* } */
+</style>
+
+
